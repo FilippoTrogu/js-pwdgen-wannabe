7ed8017a16a822da8e19d3ac0dca5c4e2c4ac06b
FilippoTrogu <filippotroguwork@gmail.com>
2023-11-21 14:38:07 +0100

    primi 3 step fatti

---
+++ script.js
@@ -0,0 +1,12 @@
+// step 1 creo la variabile contenente il nome inserito
+let name = prompt('inserisci il tuo nome');
+
+// step 2 creo la variabile contenente il cognome in serito 
+let surname = prompt('inserisci il tuo cognome');
+
+// step 3 creo la variabile contenente il colore preferito
+let color = prompt('inserisci il tuo colore preferito');
+
+//generazione password
+let password = name+surname+color+23;
+console.log(password)
